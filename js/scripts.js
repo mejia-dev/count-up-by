f95664f44d62f97717efc681a961a165c54d07ec
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-25 10:42:54 -0700

    Add functionality to not accept a 0 value
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 // Business Logic
+function isEmpty(testValue) {
+  return (testValue === 0 || testValue === null);
+}
+
+
 function countUpBy() {
   event.preventDefault();
   const countTo = parseInt(document.getElementById("countTo").value);
   const countBy = parseInt(document.getElementById("countBy").value);
-  let result = 0;
-  for (let i = 0; i <= countTo; i +=countBy) {
-    result = i;
-    console.log(i);
-    printResult(result);
+  if (isEmpty(countTo) || isEmpty(countBy)) {
+      printResult("input a different number");
+  } else {
+    let result = 0;
+    for (let i = 0; i <= countTo; i +=countBy) {
+      result = i;
+      console.log(i);
+      printResult(result);
+    }
   }
 }
+
 
 // UI Logic
 window.addEventListener("load", function () {
